b918a0a0e82ee8fabf4957cad555d27234b4140f
Jasper_Marichal <jasper.marichal@student.kdg.be>
2022-11-21 14:04:57 +0100

    added associations for classes: Board and Game

--- _2048Application/src/be/kdg/integration1/team01/game2048/model/Board.java
+++ _2048Application/src/be/kdg/integration1/team01/game2048/model/Board.java
@@ -1,13 +1,18 @@
 package be.kdg.integration1.team01.game2048.model;
 
+import java.util.ArrayList;
+
 public class Board {
     private int size;
+    private ArrayList<Block> block;
 
     public Board(int size) {
         this.size = size;
+        this.block = new ArrayList<>();
     }
 
     public int getSize() {return size;}
-
     public void setSize(int size) {this.size = size;}
+    public ArrayList<Block> getBlock() {return block;}
+    public void setBlock(ArrayList<Block> block) {this.block = block;}
 }
--- _2048Application/src/be/kdg/integration1/team01/game2048/model/Game.java
+++ _2048Application/src/be/kdg/integration1/team01/game2048/model/Game.java
@@ -3,17 +3,18 @@ package be.kdg.integration1.team01.game2048.model;
 public class Game {
     private int currentScore;
     private int turn;
+    private Board board;
 
-    public Game(int currentScore, int turn) {
+    public Game(int currentScore, int turn, Board board) {
         this.currentScore = currentScore;
         this.turn = turn;
+        this.board = board;
     }
 
     public int getCurrentScore() {return currentScore;}
-
     public void setCurrentScore(int currentScore) {this.currentScore = currentScore;}
-
     public int getTurn() {return turn;}
-
     public void setTurn(int turn) {this.turn = turn;}
+    public Board getBoard() {return board;}
+    public void setBoard(Board board) {this.board = board;}
 }
